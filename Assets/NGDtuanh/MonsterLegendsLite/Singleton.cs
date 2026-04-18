f9dee215af71742be454006ed0e096cabf5dcf28
Nguyễn Duy Tuấn Anh <ntanhpro1221@gmail.com>
2026-04-18 15:19:09 +0700

    Add singleton, remove unused script (sprite renderer ext)

---
+++ Assets/NGDtuanh/MonsterLegendsLite/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace NGDtuanh.MonsterLegends {
+    public class Singleton<T> : MonoBehaviourExt where T : MonoBehaviour {
+        private static T ins;
+
+        public static T Ins {
+            get {
+                if (ins != null) return ins;
+
+                ins = FindAnyObjectByType<T>(FindObjectsInactive.Include);
+                if (ins != null) return ins;
+
+                ins = new GameObject(typeof(T).Name + " (Singleton)").AddComponent<T>();
+
+                return ins;
+            }
+        }
+
+        protected virtual void Awake() {
+            if (ins == null
+             || ins == this) {
+                ins = this as T;
+                DontDestroyOnLoad(gameObject);
+            } else Destroy(gameObject);
+        }
+    }
+}
