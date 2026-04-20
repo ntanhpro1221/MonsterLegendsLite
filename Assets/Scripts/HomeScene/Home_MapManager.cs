21dcc726c3c36f20c0c47752448634286e464e24
Nguyễn Duy Tuấn Anh <ntanhpro1221@gmail.com>
2026-04-20 20:04:57 +0700

    Home scene: fix grid convert method + fix building's pivot

--- Assets/Scripts/HomeScene/Home_MapManager.cs
+++ Assets/Scripts/HomeScene/Home_MapManager.cs
@@ -1,19 +1,18 @@
 ﻿using NGDtuanh.MonsterLegends;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 namespace MonsterLegendsLite {
     public class Home_MapManager : Singleton<Home_MapManager> {
         [SerializeField, Required]
-        private Tilemap tilemap;
+        private Grid grid;
 
         public Vector2 GetWorldPos(Vector2Int tilePos) {
-            return tilemap.CellToWorld(new Vector3Int(tilePos.x, tilePos.y, 0));
+            return grid.GetCellCenterWorld(new Vector3Int(tilePos.x, tilePos.y, 0));
         }
 
         public Vector2 RandomPointInHabitat(Vector2Int pos, Vector2Int size) {
-            return tilemap.LocalToWorld(tilemap.CellToLocalInterpolated(new Vector3(
+            return grid.LocalToWorld(grid.CellToLocalInterpolated(new Vector3(
                 pos.x + Random.Range(0, size.x)
               , pos.y + Random.Range(0, size.y)
               , 0)));
