d37102a7a759e59481e83e249d8800541fbafdeb
Nguyễn Duy Tuấn Anh <ntanhpro1221@gmail.com>
2026-05-17 20:40:30 +0700

    Add fpm to farm info window

--- Assets/Scripts/Scenes/Home/UI/BuildingInfoWindow/Concretes/FarmInfoWindow.cs
+++ Assets/Scripts/Scenes/Home/UI/BuildingInfoWindow/Concretes/FarmInfoWindow.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 namespace MonsterLegendsLite {
     public class FarmInfoWindow : BuildingInfoWindow {
         [SerializeField, Required]
+        private UI_SpecStat_TextIcon foodPerMin;
+        
+        [SerializeField, Required]
         private UI_SpecStat_TextIcon maxFood;
 
         public static FarmInfoWindow Show(FarmInfoWindow prefab, FarmInsData target) {
             var window  = BuildingInfoWindow.Show(prefab,target);
             var defData = DataManager.Ins.GameDef.Farms[target.To<FarmInsData>().Id];
 
+            window.foodPerMin.SetText(window.utils.ToStr_Resource(defData.FoodPerMin));
             window.maxFood.SetText(window.utils.ToStr_Resource(defData.MaxFood));
 
             return window;
